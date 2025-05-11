84bd7bfc20e839285c81c8bdd3c1dba020de919d
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-05-11 14:02:42 -0700

    Add files via upload
    temporary routes for homepage

---
+++ bookstore/src/main/java/com/bookstore/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.gcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; 
+    }
+
+    @GetMapping("/books")
+    public String books() {
+        return "books"; 
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "cart"; 
+    }
+
+  
+    @GetMapping("/register")
+    public String register() {
+        return "register"; 
+    }
+    
+    @GetMapping("/categories")
+    public String categories() {
+        return "categories"; 
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact"; 
+    }
+
+}
+
+
+
