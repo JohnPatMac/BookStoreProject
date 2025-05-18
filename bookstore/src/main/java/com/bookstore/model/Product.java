d97533f04cc75363301367da9d48965fc988eff3
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-05-18 15:36:00 -0700

    Add files via upload

---
+++ bookstore/src/main/java/com/bookstore/model/Product.java
@@ -0,0 +1,81 @@
+package com.bookstore.model;
+
+import jakarta.validation.constraints.*;
+
+
+public class Product {
+
+   @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    @NotBlank(message = "Genre is required")
+    private String genre;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be positive")
+    private Double price;
+
+    @NotBlank(message = "ISBN is required")
+    private String isbn;
+
+    @NotBlank(message = "Format is required")
+    private String format;
+
+    @NotNull(message = "Stock availability is required")
+    @Min(value = 0, message = "Stock cannot be negative")
+    private Integer stock;
+
+  
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    public String getFormat() {
+        return format;
+    }
+    public void setFormat(String format) {
+        this.format = format;
+    }
+    
+    public Integer getStock() {
+        return stock;
+    }
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
