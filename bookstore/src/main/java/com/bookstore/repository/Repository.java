866f1b30e88499b65b89a1779a1d3562cf2f623c
JohnPatMac <johnp.mcintyre12@gmail.com>
2025-05-25 09:25:17 -0400

    Stop tracking .metadata folder

---
+++ bookstore/src/main/java/com/bookstore/repository/Repository.java
@@ -0,0 +1,11 @@
+package com.bookstore.repository;
+
+import com.bookstore.model.User;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repository extends CrudRepository<User, Long> {
+
+    @Query("SELECT * FROM users WHERE username = :username")
+    User findByUsername(String username);
+}
