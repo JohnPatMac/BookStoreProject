2f0f8898a9bbb2fa5e66e87b1f5c7c052a81e24f
Danielle D <daniellerdesilvio@gmail.com>
2025-05-26 16:37:45 -0400

    Resolve merge conflicts
    Merging into fork first to test that the additions work

--- bookstore/src/main/java/com/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/bookstore/BookstoreApplication.java
@@ -2,13 +2,12 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories(basePackages = "com.bookstore.repository")
 public class BookstoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
 }
