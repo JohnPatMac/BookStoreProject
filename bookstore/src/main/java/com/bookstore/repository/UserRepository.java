3b3a680fca58540be4676fae6463eed789a801c8
Danielle D <daniellerdesilvio@gmail.com>
2025-05-26 00:43:58 -0400

    annotations

--- bookstore/src/main/java/com/bookstore/repository/UserRepository.java
+++ bookstore/src/main/java/com/bookstore/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 // Repo extends Crud
-public interface Repository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     @Query("SELECT * FROM users WHERE username = :username")
     User findByUsername(String username);
