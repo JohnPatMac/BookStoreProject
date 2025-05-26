b39e5e2fb334e9ec9dc1c466c5407454bb2541e8
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-05-25 17:23:10 -0700

    Update Product.java

--- bookstore/src/main/java/com/bookstore/model/Product.java
+++ bookstore/src/main/java/com/bookstore/model/Product.java
@@ -1,11 +1,19 @@
 package com.bookstore.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 import jakarta.validation.constraints.*;
 
-
+@Table("products") 
 public class Product {
 
-   @NotBlank(message = "Title is required")
+    @Id
+    private Long id;
+
+    @NotBlank(message = "ISBN is required")
+    private String isbn;
+
+    @NotBlank(message = "Title is required")
     private String title;
 
     @NotBlank(message = "Author is required")
@@ -18,9 +26,6 @@ public class Product {
     @DecimalMin(value = "0.0", inclusive = false, message = "Price must be positive")
     private Double price;
 
-    @NotBlank(message = "ISBN is required")
-    private String isbn;
-
     @NotBlank(message = "Format is required")
     private String format;
 
@@ -28,7 +33,13 @@ public class Product {
     @Min(value = 0, message = "Stock cannot be negative")
     private Integer stock;
 
-  
+
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
     public String getTitle() {
         return title;
@@ -36,42 +47,35 @@ public class Product {
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public String getAuthor() {
         return author;
     }
     public void setAuthor(String author) {
         this.author = author;
     }
-    
+
     public String getGenre() {
         return genre;
     }
     public void setGenre(String genre) {
         this.genre = genre;
     }
-    
+
     public Double getPrice() {
         return price;
     }
     public void setPrice(Double price) {
         this.price = price;
     }
-    
-    public String getIsbn() {
-        return isbn;
-    }
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-    
+
     public String getFormat() {
         return format;
     }
     public void setFormat(String format) {
         this.format = format;
     }
-    
+
     public Integer getStock() {
         return stock;
     }
