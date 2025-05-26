3226508e136396a723e9b0ec293d00cb4ab63a6a
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-05-25 21:16:30 -0700

    Update ProductRepository.java

--- bookstore/src/main/java/com/bookstore/repository/ProductRepository.java
+++ bookstore/src/main/java/com/bookstore/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.bookstore.repository;
 
-import com.gcu.model.Product;
+import com.bookstore.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long>
