838d5a9a38cc71aa7ef4a8efb19b8bf67bb03651
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-05-25 17:24:20 -0700

    Update ProductRepository.java

--- bookstore/src/main/java/com/bookstore/repository/ProductRepository.java
+++ bookstore/src/main/java/com/bookstore/repository/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.gcu.repository;
+package com.bookstore.repository;
 
 import com.gcu.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long>
 {
-	//boolean existsByIsbn(String isbn);
+	
 }
