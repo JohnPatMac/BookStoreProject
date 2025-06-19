357a114050667a1f58da9373eec3415ea6ff3058
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-06-18 19:04:08 -0700

    Update UserLogin.java

--- bookstore/src/main/java/com/bookstore/model/UserLogin.java
+++ bookstore/src/main/java/com/bookstore/model/UserLogin.java
@@ -3,35 +3,61 @@ package com.bookstore.model;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
+/**
+ * Represents the data required for a user login attempt.
+ * Used for validating login form input.
+ */
 public class UserLogin {
 
-   
+    /**
+     * The username entered by the user.
+     * Must not be empty and must be between 3 and 50 characters.
+     */
     @NotEmpty(message = "Username is required")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") 
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
 
-  
+    /**
+     * The password entered by the user.
+     * Must not be empty and must be at least 6 characters long.
+     */
     @NotEmpty(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters long") 
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
- 
-
+    /**
+     * Gets the username.
+     *
+     * @return the entered username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username.
+     *
+     * @param username the username to set
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the password.
+     *
+     * @return the entered password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password.
+     *
+     * @param password the password to set
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 }
-
-
