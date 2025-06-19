e8bce895da26b69198fc66eb8d08b736879dcf26
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-06-18 18:52:38 -0700

    Update SwaggerConfig.java

--- bookstore/src/main/java/com/bookstore/SwaggerConfig.java
+++ bookstore/src/main/java/com/bookstore/SwaggerConfig.java
@@ -5,9 +5,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class to set up Swagger/OpenAPI documentation
+ * for the Bookstore REST API.
+ */
 @Configuration
 public class SwaggerConfig {
 
+    /**
+     * Creates the OpenAPI bean to configure API documentation details
+     * such as title, description, and version.
+     *
+     * @return the configured OpenAPI instance
+     */
     @Bean
     public OpenAPI bookstoreOpenAPI() {
         return new OpenAPI()
