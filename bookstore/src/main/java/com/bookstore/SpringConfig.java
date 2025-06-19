76f40cb895d19b0e7ee97e35066c8cfaa1655c10
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-06-18 18:52:10 -0700

    Update SpringConfig.java

--- bookstore/src/main/java/com/bookstore/SpringConfig.java
+++ bookstore/src/main/java/com/bookstore/SpringConfig.java
@@ -1,22 +1,35 @@
 package com.bookstore;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.bookstore.model.UserLogin;
 import com.bookstore.model.UserRegister;
 
+/**
+ * Spring configuration class that defines beans for
+ * UserLogin and UserRegister model objects.
+ */
 @Configuration
 public class SpringConfig {
-	
-	@Bean(name="userLogin")
-	public UserLogin getUserLogin()
-	{
-		return new UserLogin();
-	}
-	
-	@Bean(name="userRegister")
-	public UserRegister getUserRegister()
-	{
-		return new UserRegister();
-	}
+    
+    /**
+     * Creates and returns a new UserLogin bean.
+     *
+     * @return a new UserLogin instance
+     */
+    @Bean(name = "userLogin")
+    public UserLogin getUserLogin() {
+        return new UserLogin();
+    }
+    
+    /**
+     * Creates and returns a new UserRegister bean.
+     *
+     * @return a new UserRegister instance
+     */
+    @Bean(name = "userRegister")
+    public UserRegister getUserRegister() {
+        return new UserRegister();
+    }
 }
