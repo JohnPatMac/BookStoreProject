ffd29026b6859a95edd0312747d9fee878f635b1
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2025-06-18 18:54:23 -0700

    Update CustomUserDetailsService.java

--- bookstore/src/main/java/com/bookstore/service/CustomUserDetailsService.java
+++ bookstore/src/main/java/com/bookstore/service/CustomUserDetailsService.java
@@ -1,24 +1,44 @@
 package com.bookstore.service;
 
-import com.bookstore.model.User;
-import com.bookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import com.bookstore.model.User;
+import com.bookstore.repository.UserRepository;
+
 import java.util.Collections;
 
+/**
+ * Custom implementation of {@link UserDetailsService} to load user-specific data.
+ * This service is used by Spring Security during authentication to retrieve user details from the database.
+ */
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    /**
+     * Repository to access user data.
+     */
     private final UserRepository userRepository;
 
+    /**
+     * Constructs a new CustomUserDetailsService with the given UserRepository.
+     * 
+     * @param userRepository the repository used to find users by username
+     */
     @Autowired
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Locates the user based on the username.
+     * 
+     * @param username the username identifying the user whose data is required
+     * @return a fully populated {@link UserDetails} object (from Spring Security)
+     * @throws UsernameNotFoundException if the user could not be found
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
